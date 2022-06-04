fd0b9e7ec6317ea66cd80fa6fef4ce05bff26e02
Hayley <13193010680@163.com>
2022-06-04 17:37:17 +0800

    try

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.less';
+import { Redirect } from 'umi';
+
+export default function IndexPage() {
+  return (
+    <div>
+      {/* <h1 className={styles.title}>Page index</h1> */}
+      <Redirect to='/film'></Redirect>
+    </div>
+  );
+}
